5ee70a7f8e3dd8998b88909aa1a7959f126eede8
Duco <duco@winterwerp.it>
2016-04-27 21:06:21 +0200

    Updates for Chocolatey

---
+++ choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+Install-ChocolateyZipPackage 'dotlocal' 'https://github.com/dukeofharen/dotlocal/releases/download/v[VERSION]/dotlocal.zip'  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"

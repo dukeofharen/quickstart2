140d83162cd03c5d13abff25d253e02926b38fea
Duco <duco@winterwerp.it>
2016-07-02 14:52:52 +0200

    Fixed choco install file

--- choco/tools/chocolateyinstall.ps1
+++ choco/tools/chocolateyinstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyZipPackage 'quickstart2' 'https://github.com/dukeofharen/quickstart2/releases/download/v[VERSION]/quickstart2.zip'  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'quickstart2' 'https://github.com/dukeofharen/quickstart2/releases/download/v[VERSION]/qs2.zip'  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
